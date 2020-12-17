b47bb1e3d291f79d6d62028c929ab9c8da8f54b1
Sergio Ricardo <sergio.queiroz@gigalink.net.br>
2020-12-17 18:02:14 -0300

    estrutura básica programa em C

---
+++ basico001/estrutura001.c
@@ -0,0 +1,9 @@
+/*
+	ESTRUTURA BÁSICA V1
+*/
+
+#include <stdio.h>
+
+void main(){
+	printf("%s\n", "Estrutura básica 001.");
+}
---
+++ basico001/estrutura002.c
@@ -0,0 +1,11 @@
+/*
+	ESTRUTURA BÁSICA PROGRAMA C
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	printf("%s\n", "Estrutura básica 002");
+	return 0;
+}
